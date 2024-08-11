d79d616495ee4a8b49453efa6f45f79b0c6026dc
Liza-des <lizades@bk.ru>
2024-08-11 17:23:17 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours / completed
+print ('Курс:',course_name,'всего задач:',completed,'затрачено часов:',number_of_hours,'среднее время выполнения',time_for_one_task)
